22106477ef626ba99ea72cfe2aeaafdf635e3917
Laszlo-Lazuer <visualjazz@gmail.com>
2018-03-12 15:57:35 -0500

    Completes another example.

--- react-playground/src/RefLocalState/RefToStateLocal.js
+++ react-playground/src/RefLocalState/RefToStateLocal.js
@@ -4,33 +4,43 @@ class SearchLocal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      query: ' ',
-    }
+      query: ''
+    };
+    // this.onChange = this.onChange.bind(this);
+    // this.onSubmit = this.onSubmit.bind(this);
   }
 
+  // onChange(event) {
   onChange = (event) => {
-    const { value } = event.target;
-
-    this.setState({
-      query: value,
+      const { value } = event.target;
+      this.setState({ query: value
     });
   }
 
-  onSubmit = ()
+  // onSubmit(event) {
+  onSubmit = (event) => {
+
+    const { query } = this.state;
+    // do something with the search value
+    // e.g. propagate it up to the parent component
+    this.props.onSearch(query); //Disable to test input
+    // console.log(query);
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div>
-        <h2>Example Ref component</h2>
+        <h2>Example Local State component</h2>
         <form onSubmit={this.onSubmit}>
           <input
-            ref={node => this.input = node}
-            type='text'
-          />
-          <button type='submit'>
-            search
-          </button>
-        </form>
+            onChange={this.onChange}
+            type="text"
+        />
+        <button type="submit">
+          Search
+        </button>
+      </form>
       </div>
     );
   }
